71e8d345597916d687eec6611d2efc1b1a89f44f
Nick Gorbunov <intsurfer73@gmail.com>
2025-03-24 20:02:21 +0300

    Раздел 4

--- template/src/modules/constructor/DoughSelector.vue
+++ template/src/modules/constructor/DoughSelector.vue
@@ -12,10 +12,10 @@
             <input
               type="radio"
               name="dough"
-              :value="doughType.value"
-              :checked="doughType.value === modelValue"
+              :value="doughType.id"
+              :checked="doughType.id === modelValue"
               class="visually-hidden"
-              @input="emit('update:modelValue', doughType.value)"
+              @input="emit('update:modelValue', doughType.id)"
             />
             <img :src="getImage(doughType.image)" :alt="doughType.name" />
   
@@ -30,7 +30,7 @@
   <script setup>
   defineProps({
     modelValue: {
-      type: String,
+      type: Number,
       default: "",
     },
     items: {
